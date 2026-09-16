f9ed266a5ce4d5747c54bf3b145c942fd39665ad
Dubestephane1 <dubestephane@vivaldi.net>
2026-09-15 23:03:55 -0400

    completed ci build

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -146,7 +146,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
-         
+
     def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
@@ -172,4 +172,3 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-        
